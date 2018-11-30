9c6213a9c14f0f8ece13d49d56976b7d77049435
Jack Knight <jkknight9@gmail.com>
2018-11-30 14:50:41 -0700

    Initial Commit

---
+++ buttonCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buttonCounter
+//
+//  Created by Jack Knight on 11/30/18.
+//  Copyright © 2018 Jack Knight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
